86450e27a9eda8a8b4bc574786d853d84169f725
JAM48 <jihad_ahmed_mustafa@outlook.com>
2019-07-29 10:51:27 +0200

    harpReplacement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { PolicyComponent } from './policy/policy.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { OfferComponent } from './offer/offer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    PolicyComponent,
+    ContactComponent,
+    HomeComponent,
+    OfferComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'about', component: AboutComponent },
+      { path: 'contact', component: ContactComponent },
+      { path: 'policy', component: PolicyComponent},
+      { path: 'offer', component: OfferComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
